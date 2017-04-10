9ae0c0681178e9e1eac2b4157a965ac68d73199e
Abdel-latif <amatroud0@hotmail.com>
2017-04-10 10:49:13 +0200

    page d'accueil

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,17 @@
-        </div> <!-- <div class="container"> -->
+        </div> <!--<div class="container"> -->
+
+        <footer>
+            <hr>
+            <div>
+                <div class="contain">
+                    <p>&copy; 2017 CDC.</p>
+                </div>
+            </div>
+        </footer>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     </body>
 </html>
