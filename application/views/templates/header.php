78cf0edc86862aedb4f5b9b5af318727aeb0aaaa
Elyes <elyes.elbahri77@gmail.com>
2017-04-10 10:17:09 +0200

    assets / templates

---
+++ application/views/templates/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Bookerz</title>
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href=""> <!-- bootstrap link -->
+    </head>
+    <body>
+        <!-- navbar -->
+        <div class="container">
